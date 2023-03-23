1496a5c87ae81e6cfde407b25da3899ed7887402
Amajuoyi chidera <Amajuoyichideradavid@gmail.com>
2023-03-23 05:58:18 -0700

    first version

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
+import { useState } from "react";
 import './App.css';
 
+
 function App() {
+  const [car, setCar] = useState ({
+    brand:'volvo', 
+    color: 'black'
+  })
+
+  function changeBrand () {
+    setCar ({
+      ...car,
+      brand: 'benz'
+    })
+  };
+
+  const [count, setCount] = useState(0);
+
+  function increaseCount () {
+    setCount (count + 1)
+  };
+
+  function decreaseCount () {
+    setCount (count - 1)
+  };
+   
+  const [value, setValue] = useState('david')
+
+  function changeValue (event) {
+    const changed = event.target.value;
+    setValue (changed);
+  }
+
+
+  const [toggle, setToggle] = useState(false)
+
+  function toggleButton () {
+    setToggle (!toggle)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>{count}</p>
+      <button onClick={increaseCount}>increase</button>
+      <button onClick={decreaseCount}>decrease</button>
+
+      <h1>{car.brand}</h1>
+      <p>{car.color}</p>
+
+      <button onClick={changeBrand}>change car brand</button>
+
+      <input type="text" placeholder="type anything" onChange={changeValue} />
+      <h1>{value}</h1>
+
+      <button onClick={toggleButton}>toggleMe</button>
+      
+      {
+        toggle && <h1>I love my Family</h1>
+      }
+      
     </div>
   );
 }
